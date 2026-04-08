69eee8d0bf6fbb5b02efe794f02b6e5331ed2b18
Marty <martin@sayver.net>
2026-04-08 21:41:12 +0200

    SAO added hollow mission

--- sword-art-online-re-hollow-fragment/docs/hollow_missions.js
+++ sword-art-online-re-hollow-fragment/docs/hollow_missions.js
@@ -1769,7 +1769,7 @@ const HOLLOW_MISSIONS_DATA = [
   },
 
   {
-    id: "AB-48", region: "Abandoned_Area", type: "grand_quest",
+    id: "AB-48", region: "Abandoned_Area", type: "grand_quest", Part: 1,
     name: "Infestation Investigation",
     location: "Erupting Ley Lines",
     mission_rank: "Rank 1",
@@ -1782,7 +1782,7 @@ const HOLLOW_MISSIONS_DATA = [
   },
 
   {
-    id: "AB-49", region: "Abandoned_Area", type: "grand_quest",
+    id: "AB-49", region: "Abandoned_Area", type: "grand_quest", Part: 2,
     name: "Stop the Eruptive Ceremony!",
     location: "Strait of Ashes",
     mission_rank: "Rank 1",
@@ -1795,7 +1795,7 @@ const HOLLOW_MISSIONS_DATA = [
   },
 
   {
-    id: "AB-50", region: "Abandoned_Area", type: "grand_quest",
+    id: "AB-50", region: "Abandoned_Area", type: "grand_quest", Part: 3,
     name: "Stop the toxic Gas Leak!",
     location: "Effervescent Path",
     mission_rank: "Rank 1",
@@ -1808,7 +1808,7 @@ const HOLLOW_MISSIONS_DATA = [
   },
 
   {
-    id: "AB-51", region: "Abandoned_Area", type: "grand_quest",
+    id: "AB-51", region: "Abandoned_Area", type: "grand_quest", Part: 1,
     name: "Frozen Tree Cull",
     location: "Site of the Black ritual",
     mission_rank: "Rank 1",
@@ -1818,6 +1818,19 @@ const HOLLOW_MISSIONS_DATA = [
     target: "Resonant Ice Trees",
     time_limit: null, recommended_level: 112,
     image: "Images/Hollow_Missions/Abandoned_Area/Abandoned_Frozen_Tree_Cull.png",
+  },
+
+  {
+    id: "AB-52", region: "Abandoned_Area", type: "grand_quest", Part: 2,
+    name: "Snowy Mystery",
+    location: "Icetomb Road",
+    mission_rank: "Rank 1",
+    strategy: "The mountain road is teeming with ice scorpions attempting to draw power from the snow there. Defeat them and travel to the southeast ice tree to investigate the mysterious snowy power.",
+    summary: "Investigate the source of the mysterious snowy power.",
+    goal: "Defeat specific no. of target enemy/Reach goal",
+    target: "Frost Shell Stinger",
+    time_limit: null, recommended_level: 115,
+    image: "Images/Hollow_Missions/Abandoned_Area/Abandoned_Snowy_Mystery.png",
   }
 
   // ─── Administration Area ──────────────────────────────────────────────────
